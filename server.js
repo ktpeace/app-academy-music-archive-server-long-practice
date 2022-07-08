44d1578548f18356451a745d1b1f6c7d9fe68add
Kat <75002769+ktpeace@users.noreply.github.com>
2022-07-07 23:19:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -334,6 +334,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
